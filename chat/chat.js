56029adac7e3c9936652560d1bb46178a91c0d77
ChandlerSwift <chandler@chandlerswift.com>
2014-08-08 13:37:24 -0500

    Just Kidding! *NOW* I updated the files! :)

--- chat/chat.js
+++ chat/chat.js
@@ -1,8 +1,55 @@
-/* 
-Created by: Kenrick Beckett
+// strip tags
+name = name.replace(/(<([^>]+)>)/ig,"");
 
-Name: Chat Engine
-*/
+// kick off chat
+var chat =  new Chat();
+$(function() {
+
+	 chat.getState(); 
+	 
+	 // watch textarea for key presses
+     $("#sendie").keydown(function(event) {  
+     
+         var key = event.which;  
+   
+         //all keys including return.  
+         if (key >= 33) {
+           
+             var maxLength = $(this).attr("maxlength");  
+             var length = this.value.length;  
+             
+             // don't allow new content if length is maxed out
+             if (length >= maxLength) {  
+                 event.preventDefault();  
+             }  
+          }  
+	 																																																});
+	 // watch textarea for release of key press
+	 $('#sendie').keyup(function(e) {	
+	 					 
+		  if (e.keyCode == 13) { 
+		  
+            var text = $(this).val();
+			var maxLength = $(this).attr("maxlength");  
+            var length = text.length; 
+             
+            // send 
+            if (length <= maxLength + 1) { 
+             
+		        chat.send(text, name);	
+		        $(this).val("");
+		        
+            } else {
+            
+				$(this).val(text.substring(0, maxLength));
+				
+			}	
+			
+			
+		  }
+     });
+    
+});
 
 var instanse = false;
 var state;
@@ -56,9 +103,9 @@ function updateChat(){
 			   dataType: "json",
 			   success: function(data){
 				   if(data.text){
+						beep();
 						for (var i = 0; i < data.text.length; i++) {
                             $('#chat-area').append($("<p>"+ data.text[i] +"</p>"));
-							beep();
                         }								  
 				   }
 				   document.getElementById('chat-area').scrollTop = document.getElementById('chat-area').scrollHeight;
--- chat/index.php
+++ chat/index.php
@@ -12,106 +12,68 @@ if (login_check($mysqli) == true) {
  include '../includes/config.php';
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<?php
+$page = "Chat";
+include '../includes/top.php'; ?>
+			<style>
+				body                            { background: url(bg.png); }
+				h2                              { margin: 0; padding: 0; color: #fa9f00; font: 30px Helvetica, Sans-Serif; margin: 0 0 10px 0; }
+				#page-wrap                      { padding: 0; width: 600px; margin: 30px auto; position: relative; }
 
-<head>
+				#chat-wrap                      { padding: 0; border: 1px solid #eee; margin: 0 0 15px 0; }
+				#chat-area                      { margin: 0; height: 500px; overflow: auto; border: 1px solid #666; padding: 20px; background: white; }
+				#chat-area span                 { color: white; background: #333; padding: 4px 8px; -moz-border-radius: 5px; -webkit-border-radius: 8px; margin: 0 5px 0 0; }
+				#chat-area p                    { margin: 0; padding: 8px 0; border-bottom: 1px solid #ccc; }
 
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    
-    <title>Chat (Beta) | <?php echo $sitename ; ?></title>
-    
-    <link rel="stylesheet" href="style.css" type="text/css" />
-    
-    <script type="text/javascript" src="/js/jquery.min.js"></script>
-    <script type="text/javascript" src="chat.js"></script>
-    <script type="text/javascript">
-    
-        // define username   
-        var name = "<?php echo htmlentities($_SESSION['first']); ?>";
-        
-        // default name is 'Guest'
-    	if (!name || name === ' ') {
-    	   name = "Guest";	
-    	}
-    	
-    	// strip tags
-    	name = name.replace(/(<([^>]+)>)/ig,"");
-    	
-    	// display name on page
-    	$("#name-area").html("You are: <span>" + name + "</span>");
-    	
-    	// kick off chat
-        var chat =  new Chat();
-    	$(function() {
-    	
-    		 chat.getState(); 
-    		 
-    		 // watch textarea for key presses
-             $("#sendie").keydown(function(event) {  
-             
-                 var key = event.which;  
-           
-                 //all keys including return.  
-                 if (key >= 33) {
-                   
-                     var maxLength = $(this).attr("maxlength");  
-                     var length = this.value.length;  
-                     
-                     // don't allow new content if length is maxed out
-                     //if (length >= maxLength) {  
-                     //   event.preventDefault();  
-                     //}  
-                  }  
-    		 																																																});
-    		 // watch textarea for release of key press
-    		 $('#sendie').keyup(function(e) {	
-    		 					 
-    			  if (e.keyCode == 13) { 
-    			  
-                    var text = $(this).val();
-    				var maxLength = $(this).attr("maxlength");  
-                    var length = text.length; 
-                     
-                    // send 
-                    if (length <= maxLength + 1) { 
-                     
-    			        chat.send(text, name);	
-    			        $(this).val("");
-    			        
-                    } else {
-                    
-    					$(this).val(text.substring(0, maxLength));
-    					
-    				}	
-    				
-    				
-    			  }
-             });
-            
-    	});
-    </script>
+				#name-area                      { margin: 0; padding: 0; position: absolute; top: 12px; right: 0; color: white; font: bold 12px "Lucida Grande", Sans-Serif; text-align: right; }   
+				#name-area span                 { margin: 0; padding: 0; color: #fa9f00; }
 
-</head>
+				#send-message-area p            { margin: 0; float: left; color: white; padding-top: 27px; font-size: 14px; }
+				#sendie                         { margin: 0; border: 3px solid #999; width: 460px; padding: 10px; font: 12px "Lucida Grande", Sans-Serif; float: right; }
+			</style>
+			<div id="page-wrap">
+		
+				<h2>Live Chat</h2>
+				
+				<p id="name-area"></p>
+				
+				<div id="chat-wrap"><div id="chat-area"></div></div>
+				
+				<form id="send-message-area">
+				    <p>Your message: </p>
+				    <textarea id="sendie" maxlength = '100' ></textarea>
+				</form>
+		
+			</div>
 
-<body onload="setInterval('chat.update()', 1000)">
-
-    <div id="page-wrap">
-    
-        <h2>Welcome to Minecraft Chat!</h2>
-		<h4>BETA | <?php echo $sitename ; ?></h4>
-        
-        <p id="name-area"></p>
-        
-        <div id="chat-wrap"><div id="chat-area"></div></div>
-        
-        <form id="send-message-area">
-            <p>Your message: </p>
-            <textarea id="sendie" maxlength = '1000' ></textarea>
-        </form>
-    
+        </div>
+      </div>
     </div>
-
-</body>
-
+	<script> var name = "<?php if ($logged == "in") { echo htmlentities($_SESSION['first']) ; } else { echo "Guest" ; } ?>"; </script>
+    <script type="text/javascript" src="/js/jquery.min.js"></script>
+	<script type="text/javascript" src="/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="chat.js"></script>
+	<script>
+		var setChatInt = setInterval('chat.update()', 1000);
+		$('#name-area').html('You are: <span>' + name + '</span>');
+	</script>
+	<script>
+		function serverStatus()
+		{
+			var xmlhttp = new XMLHttpRequest();
+			
+			xmlhttp.onreadystatechange=function()
+			  {
+			  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+			    {
+				    document.getElementById	("ServerStatus").innerHTML=xmlhttp.responseText;	
+			    }	
+			  }	
+			xmlhttp.open("GET","/includes/serverinfo.php",true);
+			xmlhttp.send();
+		}
+		serverStatus();
+		var setStatusInt = setInterval(serverStatus, 10000);
+	</script>
+  </body>
 </html>
